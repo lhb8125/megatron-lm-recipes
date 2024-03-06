3f29e80259ed49945cda9adb607f6346a2eb4e5c
Hongbin Liu <hongbinl@nvidia.com>
2024-03-06 03:07:35 -0800

    add scripts for llama models

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # megatron-lm-recipes
-Achieving the SOL performance of Megatron-LM
+Achieving the SOL training performance with Megatron-LM.
+
+Now we provide scripts for training llama2 series models.
+## Usage
+* Pull the latest commit on the main branch of Megatron-LM repo
+  ```
+  git clone https://github.com/NVIDIA/Megatron-LM.git
+  ```
+* Pull the latest commit on the main branch of TransformerEngine repo and install
+  ```
+  git clone https://github.com/NVIDIA/TransformerEngine.git
+  ```
+* Create a config file including your home path and wanbd api key
+  ```
+  !/bin/bash
+  HOME_PATH=***
+  WANDB=***
+  ```
+* Modify the relative path in the scripts
+* Prepare the dataset following the instructions [here](https://github.com/NVIDIA/Megatron-LM/tree/main/megatron?tab=readme-ov-file#data-preprocessing) or use mock data
+* Run training inside NeMo Framework container v24.01.
+  ```
+  nvcr.io/ea-bignlp/ga-participants/nemofw-training:24.01
+  ```
+
+## Note
+* The scripts are designed for slurm environment, and coordinated with the related slurm scripts. Hence, for other environments like K8s, you need to modify the launch cmd for running on multiple nodes.
+* We verified the performance on the following commits
+  * Megatron-LM: ```361c97d4bb8f749fd55035705a811616ec085ed2 (2/20/2024)```
+  * TransformerEngine: ```2187a8f3bbadff7fb9922b85473a6135f8272fdc(2/20/2024)```
---
+++ llama/llama2_7b_bf16.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+set -x
+
+export CUDA_DEVICE_MAX_CONNECTIONS=1
+#export NVTE_TORCH_COMPILE=0
+export TORCH_NCCL_AVOID_RECORD_STREAMS=1
+#export NCCL_IB_SL=1
+#export NCCL_IB_TIMEOUT=19
+export NVTE_FWD_LAYERNORM_SM_MARGIN=8
+export NVTE_BWD_LAYERNORM_SM_MARGIN=8
+export NCCL_NVLS_ENABLE=0
+export NVTE_DP_AMAX_REDUCE_INTERVAL=0
+export NVTE_ASYNC_AMAX_REDUCTION=1
+export NVTE_FUSED_ATTN=0
+
+source /home/hongbinl/private.cfg # HOME_PATH and WANDB key
+
+EXP_NAME=llama2-7b-bf16-mlm
+CHECKPOINT_PATH=$HOME_PATH/fp8/checkpoints/llama2/text/7b-mcore-bf16-perf
+TOKENIZER_MODEL=$HOME_PATH/fp8/checkpoints/llama2/tokenizer.model
+#DATA_PATH=$HOME_PATH/datasets/pile/llama
+#DATA_PATH=$HOME_PATH/datasets/RedPajamaV2
+DATA_PATH=$HOME_PATH/datasets/llama/RedPajamaV2/merged
+MLM_PATH=$HOME_PATH/fp8/perf/Megatron-LM
+TE_PATH=$HOME_PATH/fp8/perf/TransformerEngine
+
+source ${DATA_PATH}/llama_blend.sh
+
+export PYTHONPATH=${MLM_PATH}:${TE_PATH}:${PYTHONPATH}
+
+LLAMA_ARGS="
+    --num-layers 32 \
+    --hidden-size 4096 \
+    --ffn-hidden-size 11008 \
+    --num-attention-heads 32 \
+    --seq-length 4096 \
+    --max-position-embeddings 4096 \
+    --global-batch-size 128 \
+    --untie-embeddings-and-output-weights \
+    --swiglu \
+    --position-embedding-type rope \
+    --rotary-percent 1.0 \
+    --disable-bias-linear \
+    --no-position-embedding \
+    --normalization RMSNorm \
+    --make-vocab-size-divisible-by 128 \
+    --norm-epsilon 1e-5 \
+    --lr 3e-4 \
+    --train-iters 350000 \
+    --lr-decay-style cosine \
+    --lr-warmup-iters 2000 \
+    --min-lr 3e-5 \
+    --weight-decay 0.1 \
+    --adam-beta1 0.9 \
+    --adam-beta2 0.95 \
+    --adam-eps 1e-5 \
+    --clip-grad 1.0 \
+    --attention-dropout 0.0 \
+    --hidden-dropout 0.0 \
+"
+
+DATA_ARGS="
+    --data-path ${DATA_BLEND} \
+    --tokenizer-type Llama2Tokenizer \
+    --tokenizer-model ${TOKENIZER_MODEL} \
+    --split 990,8,2 \
+"
+
+OUTPUT_ARGS="
+    --log-interval 10 \
+    --save-interval 250 \
+    --eval-interval 1000 \
+    --eval-iters 20 \
+    --timing-log-level 0 \
+    --wandb-project mlm_llama_perf \
+    --wandb-exp-name ${EXP_NAME} \
+    --wandb-save-dir $HOME_PATH/fp8/results/${EXP_NAME} \
+    --tensorboard-dir $HOME_PATH/fp8/results/${EXP_NAME} \
+    --tensorboard-log-interval 10 \
+    --log-timers-to-tensorboard \
+    --log-batch-size-to-tensorboard \
+    --log-validation-ppl-to-tensorboard \
+    --log-memory-to-tensorboard \
+"
+
+FP8_ARGS="
+    --fp8-format hybrid \
+    --fp8-margin 0 \
+    --fp8-interval 1 \
+    --fp8-amax-history-len 1024 \
+    --fp8-amax-compute-algo max \
+"
+OTHER_ARGS="
+    --micro-batch-size 1 \
+    --tensor-model-parallel-size 1 \
+    --pipeline-model-parallel-size 1 \
+    --bf16 \
+    --use-flash-attn \
+    --sequence-parallel \
+    --use-mcore-models \
+    --use-distributed-optimizer \
+    --transformer-impl transformer_engine \
+    --no-check-for-nan-in-loss-and-grad \
+    --overlap-grad-reduce \
+    --overlap-param-gather \
+    --tp-comm-overlap \
+"
+PROFILE_ARGS="
+    --profile \
+    --profile-step-start 50 \
+    --profile-step-end 60 \
+"
+
+wandb login ${WANDB}
+nsys profile -s none -t nvtx,cuda -o $HOME_PATH/fp8/perf/${EXP_NAME} --force-overwrite true --capture-range=cudaProfilerApi --capture-range-end=stop \
+python ${MLM_PATH}/pretrain_gpt.py \
+    ${LLAMA_ARGS} \
+    ${DATA_ARGS} \
+    ${OUTPUT_ARGS} \
+    ${OTHER_ARGS} \
+    --save ${CHECKPOINT_PATH} \
+    --distributed-backend nccl
